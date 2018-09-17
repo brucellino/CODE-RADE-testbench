16034766d8b53fdba908ccdc4454fd166fca536a
Bruce Becker <brucellino@gmail.com>
2018-09-17 14:32:00 +0200

    Initial commit

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,38 @@
+import os
+import pytest
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+@pytest.mark.parametrize("p", ["python",
+                               "python36",
+                               "python-setuptools",
+                               "python36-setuptools"])
+def test_python_installed(host, p):
+    pkg = host.package(p)
+    assert pkg.is_installed
+
+
+# @pytest.mark.parametrize("pip_package", ["pytest"])
+# def test_python_tools(host):
+#     assert host.pip_package.get_packages()
+
+
+@pytest.mark.parametrize("p", ["ruby",
+                               "ruby-devel",
+                               "ruby-irb",
+                               "ruby-libs",
+                               "rubygems"])
+def test_ruby_installed(host, p):
+    pkg = host.package(p)
+    assert pkg.is_installed
